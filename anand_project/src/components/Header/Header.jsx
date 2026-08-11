074a2d38ca64eea639d6aa566ca8b724a3dcb9c3
unknown <snadyaandrade6045@digitalcollege.internal>
2026-08-10 22:11:00 -0300

    Atualização da page e component Header

--- anand_project/src/components/Header/Header.jsx
+++ anand_project/src/components/Header/Header.jsx
@@ -4,8 +4,11 @@ export default function Header(){
     return(
         <div className="menuNav">
             <ul>Home</ul>
-            <div className=''>
-
+            <div className="menuUser">
+                <ul className="notifica">🔔</ul>
+                <ul>
+                    <img src={'https://cdn-icons-png.flaticon.com/512/4792/4792929.png'} alt="avatarUser" />
+                </ul>
             </div>
 
         </div>
